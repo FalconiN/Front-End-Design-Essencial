5eed9a9d5554eabb969ab3c56bb71ff3b6a8ce93
Natan Falconi <nfalconi65@gmail.com>
2020-12-23 20:16:51 -0300

    navbar number counting features

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,24 @@
 //   PORTIFÓLIO SLIDER
 
 // Declarando variáveis do slider
-
 const sliderContainer = document.querySelector('.jl-slider-container')
 const sliderList = document.querySelector('.jl-slider-list')
 const sliderItem = document.querySelectorAll('.jl-slider-item')
 var sliderListWidth = null
+
+var prev = document.querySelector('.jl-item-prev')
+var next = document.querySelector('.jl-item-next')
+var sliderPos = 0
+
+const slideTotalItems = sliderItem.length
+var currentSlide = document.querySelector('.jl-current-slide')
+var totalSlide = document.querySelector('.jl-total-slide')
+var jlnumberslide = document.querySelector('.jl-number-slide')
+
+var currentCounter = 1
+
+
+var navItems = document.querySelectorAll('.jl-item-navigator a')
 
 //capturando larguras indivíduais
 var containerWidth = sliderContainer.parentElement.offsetWidth
@@ -25,12 +38,8 @@ sliderList.style.width = sliderListWidth + 'px'
 
 //fazendo animação deo Slider OnClick
 
-var prev = document.querySelector('.jl-item-prev')
-var next = document.querySelector('.jl-item-next')
-var sliderPos = 0
 
-
-// HANDLERS
+// HANDLERS  
 
 // Next slider animação
 var nextSliderAnim = function(){
@@ -64,13 +73,80 @@ var prevSliderAnim = function(){
 }
 
 
+// Counter Formater
+var counterFormater = function(n){
+    if (n <= 9){
+        return '0' + n
+    }
+    else {
+        return n
+    }
+}
+
+// Counter add
+var counterAdd = function (){
+    if(currentCounter >= 0 && currentCounter < slideTotalItems){
+        currentCounter++
+        currentSlide.innerHTML = counterFormater(currentCounter)
+        jlnumberslide.innerHTML = counterFormater(currentCounter)
+    }
+}
 
 
+// Counter Remove
+var counterRemove = function(){
+    if (currentCounter > 1  && currentCounter <= slideTotalItems){
+        currentCounter--
+        currentSlide.innerHTML = counterFormater(currentCounter)
+        jlnumberslide.innerHTML = counterFormater(currentCounter)
+    }
+}
+
+// SET ACTIVE NAV
+
+var setActiveNav = function(){
+    for (var nv = 0; nv < navItems.length; nv++){
+        let myNavNum = parseInt(navItems[nv].getAttribute('data-nav'))
+
+        if (myNavNum === currentCounter){
+            navItems[nv].classList.add('jl-item-active')
+
+            anime({
+                targets: navItems[nv],
+                width: 90
+            })
+        }
+    }
+}
+
+var changeActive = function(){
+    for(var rm = 0; rm < navItems.length; rm++){
+        navItems[rm].classList.remove('jl-item-active')
+        anime({
+            targets: navItems[rm],
+            width: 20
+        })
+    }
+
+    setActiveNav()
+}
+
+// Actions
+totalSlide.innerHTML = counterFormater(slideTotalItems)
+
+anime({
+    targets: ".jl-item-active",
+    width: 90
+})
 
 next.addEventListener('click', function(){
     nextSliderAnim()
+    counterAdd()
+    changeActive()
 })
 
 prev.addEventListener('click', function(){
     prevSliderAnim()
+    counterRemove()
+    changeActive()
 })
